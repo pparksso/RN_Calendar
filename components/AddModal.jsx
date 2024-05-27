7de6964f48d37695dcbbdf667f3ae45042aea1af
pparksso <thgml5143@hanmail.net>
2024-05-27 12:51:56 +0900

    design: modal

--- components/AddModal.jsx
+++ components/AddModal.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React from "react";
+import { Modal, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
-const AddModal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const colors = [
+  "#FF6347", // 토마토 (빨강)
+  "#FFA500", // 오렌지 (주황)
+  "#FFD700", // 골드 (노랑)
+  "#32CD32", // 라임그린 (초록)
+  "#1E90FF", // 다저블루 (파랑)
+  "#4169E1", // 로열블루 (남색)
+  "#8A2BE2", // 블루바이올렛 (보라)
+];
+
+const AddModal = props => {
+  const { visible } = props;
+  return (
+    <Modal visible={visible} animationType="fade" transparent={true}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalInner}>
+          <TextInput style={styles.input} autoFocus={true} maxLength={16} />
+          <View style={styles.colorBox}>
+            {colors.map((color, idx) => (
+              <Pressable key={idx} style={[styles.color, { backgroundColor: color }]} />
+            ))}
+          </View>
+          <View style={styles.btnBox}>
+            <Pressable style={styles.btn}>
+              <Text style={styles.btnTxt}>Save</Text>
+            </Pressable>
+            <Pressable style={styles.btn} onPress={props.close}>
+              <Text style={styles.btnTxt}>Cancle</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 };
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.3)",
+  },
+  modalInner: {
+    width: "70%",
+    borderRadius: 5,
+    backgroundColor: "#fff",
+    padding: 30,
+  },
+  input: {
+    borderBottomWidth: 1,
+    paddingVertical: 5,
+    fontSize: 20,
+  },
+  colorBox: {
+    marginTop: 20,
+    paddingHorizontal: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  color: {
+    width: 20,
+    height: 20,
+    borderRadius: 100,
+  },
+  btnBox: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  btn: {
+    backgroundColor: "#333",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
+    marginHorizontal: 10,
+  },
+  btnTxt: {
+    color: "#fff",
+    fontWeight: "700",
+    fontSize: 17,
+  },
+});
 
 export default AddModal;
--- screens/ListScreen.jsx
+++ screens/ListScreen.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image, Pressable, Text, StyleSheet } from "react-native";
 import Section from "../components/ui/Section";
 import TitleCard from "../components/ui/TitleCard";
 import Item from "../components/Item";
+import AddModal from "../components/AddModal";
 
 const ListScreen = () => {
+  const [isModal, setIsModal] = useState(false);
+
+  const openModalHandler = () => {
+    setIsModal(true);
+  };
+
+  const closeModalHandler = () => {
+    setIsModal(false);
+  };
+
   return (
-    <Section>
-      <TitleCard>
-        <View style={styles.titleContainer}>
-          <Pressable style={styles.addBtn}>
-            <Image style={styles.addImg} source={require("../assets/images/plus.png")} />
-          </Pressable>
-          <View style={styles.dateContainer}>
-            <Pressable style={styles.calendarBtn}>
-              <Image style={styles.calendarImg} source={require("../assets/images/calendar.png")} />
+    <>
+      <Section>
+        <TitleCard>
+          <View style={styles.titleContainer}>
+            <Pressable style={styles.addBtn} onPress={openModalHandler}>
+              <Image style={styles.addImg} source={require("../assets/images/plus.png")} />
             </Pressable>
-            <Text style={styles.dateText}>2024.04.09</Text>
+            <View style={styles.dateContainer}>
+              <Pressable style={styles.calendarBtn}>
+                <Image style={styles.calendarImg} source={require("../assets/images/calendar.png")} />
+              </Pressable>
+              <Text style={styles.dateText}>2024.04.09</Text>
+            </View>
           </View>
+        </TitleCard>
+        <View style={styles.listContainer}>
+          <Item checked={true} />
+          <Item />
         </View>
-      </TitleCard>
-      <View style={styles.listContainer}>
-        <Item checked={true} />
-        <Item />
-      </View>
-    </Section>
+      </Section>
+      <AddModal visible={isModal} close={closeModalHandler} />
+    </>
   );
 };
 
